a8d072a1521233c674cf33e21770b7512b5e4c1b
Daniel Monti <dms.monti@gmail.com>
2019-10-22 11:25:07 -0300

    Bootstrap dashboard template

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <core-navbar />
+    <div class="container-fluid">
+      <div class="row">
+        <core-sidebar />
+        <core-main />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {}
+}
+</script>
+<style lang="scss">
+@import 'styles/index.scss';
+.bd-placeholder-img {
+  font-size: 1.125rem;
+  text-anchor: middle;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+@media (min-width: 768px) {
+  .bd-placeholder-img-lg {
+    font-size: 3.5rem;
+  }
+}
+</style>
---
+++ src/components/core/Chart.vue
@@ -0,0 +1,61 @@
+<template>
+  <canvas ref="chart" class="chart"></canvas>
+</template>
+
+<script>
+import Chart from 'chart.js'
+
+export default {
+  methods: {
+    buildChart() {
+      var ctx = this.$refs['chart']
+      // eslint-disable-next-line no-unused-vars
+      var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: [
+            'Sunday',
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday'
+          ],
+          datasets: [{
+            data: [
+              15339,
+              21345,
+              18483,
+              24003,
+              23489,
+              24092,
+              12034
+            ],
+            lineTension: 0,
+            backgroundColor: 'transparent',
+            borderColor: '#007bff',
+            borderWidth: 4,
+            pointBackgroundColor: '#007bff'
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: false
+              }
+            }]
+          },
+          legend: {
+            display: false
+          }
+        }
+      })
+    }
+  },
+  mounted: function () {
+    this.buildChart()
+  }
+}
+</script>
